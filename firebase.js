92f5d908197bee4d5907c93f7bea2e677a1f03e1
GeorgeChristopherLopez <georgechristopherlopez@gmail.com>
2021-10-10 18:01:42 -0400

    firebase auth & firestore user db

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+ // config goes here
 };
 
 // Initialize Firebase
