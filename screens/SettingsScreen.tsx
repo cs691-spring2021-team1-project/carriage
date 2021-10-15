eb9c394f4956f75d57600ee6b665ced16d3d80e8
Jcowwell <jcowwell@gmail.com>
2021-10-15 15:11:02 -0400

    Iteration 1 documents and update an account modal

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,23 +1,54 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground, Modal, TextInput, Button } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { AuthContext } from '../components/context'
+import {Mixins} from '../src/styles/'
 
 const SettingsScreen = () => {
   const {signOut, deleteAccount} = React.useContext(AuthContext)
+
+  // This is to manage Modal State
+  const [isModalVisible, setModalVisible] = useState(false);
+  
+  // This is to manage TextInput State
+  const [inputValue, setInputValue] = useState("");
+
+  // Create toggleModalVisibility function that will
+  // Open and close modal upon button clicks.
+  const toggleModalVisibility = () => {
+      setModalVisible(!isModalVisible);
+  };
+
+  
+
     return (
         <View>
         <ImageBackground style={{height:"100%"}} source={require("../assets/MasterBG.png")}  >
 
         <View style={{margin:15}}>
+            <View>
+              <Modal animationType="slide" 
+                    transparent visible={isModalVisible} 
+                    presentationStyle="overFullScreen" 
+                    onDismiss={toggleModalVisibility}>
+                  <View style={styles.viewWrapper}>
+                      <View style={styles.modalView}>
+                          <TextInput placeholder="Enter something..." 
+                                    value={inputValue} style={styles.textInput} 
+                                    onChangeText={(value) => setInputValue(value)} />
+    
+                          {/** This button is responsible to close the modal */}
+                          <Button title="Close" onPress={toggleModalVisibility} />
+                      </View>
+                  </View>
+                </Modal>
+            </View>
             <ScrollView>
 
-
-          
             <View>
             <Text style={{fontSize:28, fontWeight:'bold', marginVertical:10}}>Account</Text>
             <View style={{elevation: 10,borderWidth:1, borderRadius:20,  paddingHorizontal:10, borderColor:'black', backgroundColor: '#f7f7f7'}}>
-                     <TouchableOpacity   >
+                      <TouchableOpacity onPress={toggleModalVisibility}>
                        <View style={{flexDirection: 'row', height: 48, alignItems: 'center'}}>
                        <Text >Change Account Information</Text>
                          <Image style={{marginHorizontal:5}} source={require('../assets/SeeMoreButton.png')} />
@@ -168,4 +199,34 @@ const SettingsScreen = () => {
 
 export default SettingsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewWrapper: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  modalView: {
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      elevation: 5,
+      transform: [{ translateX: -(Mixins.WINDOW_WIDTH * 0.4) }, 
+                  { translateY: -90 }],
+      height: 180,
+      width: Mixins.WINDOW_WIDTH * 0.8,
+      backgroundColor: "#fff",
+      borderRadius: 7,
+  },
+  textInput: {
+      width: "80%",
+      borderRadius: 5,
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+      borderColor: "rgba(0, 0, 0, 0.2)",
+      borderWidth: 1,
+      marginBottom: 8,
+  },
+});
--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,6 +1,6 @@
 import {Dimensions,PixelRatio} from 'react-native';
 
-const WINDOW_WIDTH = Dimensions.get('window').width;
+export const WINDOW_WIDTH = Dimensions.get('window').width;
 const guidelineBaseWidth = 375;
 
 export const scaleSize = size => (WINDOW_WIDTH/guidelineBaseWidth) * size;
