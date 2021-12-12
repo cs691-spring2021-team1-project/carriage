b599bee9024776a4dffbe187ba194451dc09537e
Jcowwell <Jcowwell@gmail.com>
2021-12-12 11:31:09 -0500

    item serivces

--- src/services/ItemServices.js
+++ src/services/ItemServices.js
@@ -1,3 +1,7 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { auth, firestore } from '../config';
+
+
 export const uid = () => {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
@@ -6,7 +10,22 @@ export const tempID = () => {
  return [1,2,3,4,5,6]
 }
 
-// export const favoriteItem = (itemID) => {
+export const favoriteItem = (itemID) => {
 
-// }
+}
+
+const changeMembership = async (user, new_status) => {
+    return firestore.doc(`users/${user.uid}`).update({
+        status : new_status,
+        updatedAt: new Date()
+    })
+    .then(() => {
+        console.log("User Membership Changed")
+        return true;
+    })
+    .catch((error) => {
+        console.log("Cannot Change User Membership: ", error)
+        return false;
+    })
+}
 
